b09c9cd0be9d3f2a0f4d9bd4ec38507846bc2df8
Simon Josi <simon.josi@atizo.com>
2010-11-12 22:28:19 +0100

    add new copyright notice; switch to two space indentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,9 +1,10 @@
 #
 # samba module
 #
-# Copyright 2008, Puzzle ITC
+# Copyright 2008, Puzzle ITC GmbH
+# Copyright 2010, Atizo AG
 # Marcel Härry haerry+puppet(at)puzzle.ch
-# Simon Josi josi+puppet(at)puzzle.ch
+# Simon Josi simon.josi+puppet(at)atizo.com
 #
 # This program is free software; you can redistribute 
 # it and/or modify it under the terms of the GNU 
@@ -12,23 +13,26 @@
 #
 
 class samba {
-    package{ [ 'samba', 'samba-client' ]:
-        ensure => present,
-    }
-    service{'samba':
-        name => 'smb',
-        ensure => running,
-        enable => true,
-        hasstatus => true,
-        require => Package['samba'],
-    }
-    file{'/etc/samba/smb.conf':
-        source => [ "puppet://$server/sitze-samba/$fqdn/smb.conf",
-                    "puppet://$server/site-samba/$operatingsystem/smb.conf",
-                    "puppet://$server/site-samba/smb.conf",
-                    "puppet://$server/samba/$operatingsystem/smb.conf",
-                    "puppet://$server/samba/smb.conf" ],
-        require => Package['samba'],
-        owner => root, group => 0, mode => 0644;
-    }
+  package{[ 'samba', 'samba-client' ]:
+    ensure => present,
+  }
+  service{'samba':
+    name => 'smb',
+    ensure => running,
+    enable => true,
+    hasstatus => true,
+    require => Package['samba'],
+  }
+  file{'/etc/samba/smb.conf':
+    source => [
+      "puppet://$server/modules/site-samba/$fqdn/smb.conf",
+      "puppet://$server/modules/site-samba/$operatingsystem/smb.conf",
+      "puppet://$server/modules/site-samba/smb.conf",
+      "puppet://$server/modules/samba/$operatingsystem/smb.conf",
+      "puppet://$server/modules/samba/smb.conf",
+    ],
+    require => Package['samba'],
+    notify => Service['samba'],
+    owner => root, group => 0, mode => 0644;
+  }
 }
